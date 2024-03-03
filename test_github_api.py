f1219c03affe8b9f587e8ade17591ee09dbdb376
zhenzhang0123 <zhen.zhang0123@gmail.com>
2024-03-02 20:54:32 -0500

    mock test

--- test_github_api.py
+++ test_github_api.py
@@ -1,27 +1,39 @@
-from github_api import githubApi, getRepoNameByUserId, getCommitByRepo
-import pytest
+from github_api import getRepoNameByUserId, getCommitByRepo, githubApi
+import unittest
+from unittest.mock import patch
 
-def test_getRepoNameByUserId():
-    userId = 'richkempinski'
-    repoNames = getRepoNameByUserId(userId)
-    assert repoNames == ['csp', 'hellogitworld', 'helloworld', 'Mocks', 'Project1','richkempinski.github.io','threads-of-life','try_nbdev','try_nbdev2']
+class TestGitHubAPI(unittest.TestCase):
+    @patch('github_api.requests.get')
+    def test_getRepoNameByUserId(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.text = '[{"name": "csp"}, {"name": "hellogitworld"}]'
+        
+        userId = 'richkempinski'
+        repoNames = getRepoNameByUserId(userId)
+        self.assertEqual(repoNames, ['csp', 'hellogitworld'])
 
+    @patch('github_api.requests.get')
+    def test_getCommitByRepo(self, mock_get):
+        mock_response = mock_get.return_value
+        mock_response.status_code = 200
+        mock_response.text = '[{"commit": {"message": "commit message"}}]'
+        
+        userId = 'richkempinski'
+        repoName = 'repo1'
+        commitCount = getCommitByRepo(userId, repoName)
+        self.assertEqual(commitCount, 1)
 
-def test_getCommitByRepo():
-    userId = 'richkempinski'
-    repoName = 'hellogitworld'
-    commitCount = getCommitByRepo(userId, repoName)
-    assert commitCount == 30
+    @patch('github_api.getRepoNameByUserId')
+    @patch('github_api.getCommitByRepo')
+    def test_githubApi(self, mock_getCommitByRepo, mock_getRepoNameByUserId):
+        mock_getRepoNameByUserId.return_value = ['csp', 'hellogitworld']
+        mock_getCommitByRepo.side_effect = [1, 3] 
+        
+        userId = 'richkempinski'
+        apiInfo = githubApi(userId)
+        expected_output = """Repo: csp Number of commits: 1\nRepo: hellogitworld Number of commits: 3"""
+        self.assertEqual(apiInfo, expected_output)
 
-def test_githubApi():
-    userId = 'richkempinski'
-    apiInfo = githubApi(userId)
-    assert apiInfo == """Repo: csp Number of commits: 2
-Repo: hellogitworld Number of commits: 30
-Repo: helloworld Number of commits: 6
-Repo: Mocks Number of commits: 10
-Repo: Project1 Number of commits: 2
-Repo: richkempinski.github.io Number of commits: 9
-Repo: threads-of-life Number of commits: 1
-Repo: try_nbdev Number of commits: 2
-Repo: try_nbdev2 Number of commits: 5"""
+if __name__ == '__main__':
+    unittest.main()
